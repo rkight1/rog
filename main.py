3dd083cb906a8e2a74143a5b2c84d44155bbb192
RJK <kightrobertblog@aol.com>
2025-07-05 18:25:30 -0400

    Program can now create and/or clean the 'dest' directory, copy files into the 'dest' directory, and create page dictionaries from source files.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+from shutil import rmtree, copytree
 
 # external packages
 import chevron
@@ -34,6 +36,71 @@ def renderTemplate(tName, tPath, pDict):
     return chevron.render(**args)
 
 
+# Split pages into meta and content halfs
+def splitPage(content):
+    parts = content.split("\n+++\n")
+
+    return [parts[0].strip(), parts[1].strip()]
+
+
+# Slurp a page and build a dict of page data. Also
+# validates the page to ensure required keys exist.
+def scanPage(pgPath):
+    try:
+        with open(pgPath, 'r') as f:
+            content = f.read()
+    except Exception as e:
+        print(f"ERROR: Unable to read page at {pgPath}")
+        print(f"ERROR: {e}")
+
+    parts = splitPage(content)
+
+    meta = yaml.safe_load(parts[0])
+    content = parts[1]
+
+    # At a bare minimum, all pages need a date and a title
+    if "title" not in meta:
+        print(f"ERROR: Page at '{pgPath}' has no TITLE!")
+        sys.exit(1)
+
+    if "date" not in meta:
+        print(f"ERROR: Page at '{pgPath}' has no DATE!")
+        sys.exit(1)
+
+    # Other keys can have default values
+    if "tags" not in meta:
+        meta["tags"] = []
+
+    if "template" not in meta:
+        meta["template"] = "default"
+
+    meta["content"] = content
+
+    return meta
+
+
+def getFiles(path):
+    fileList = []
+    for (root,dirs,files) in os.walk('src',topdown=True):
+        for f in files:
+            fileList.append(f"{root}/{f}")
+
+    return fileList
+
+
+def scanPages(dir):
+    allFiles = getFiles('dest')
+    pages = []
+
+    for a in allFiles:
+        if a.endswith(".md"):
+            pDict = scanPage(a)
+            pages.append(pDict)
+
+    return pages
+
+
+
 def main():
     # First, load the config file
     try:
@@ -43,7 +110,24 @@ def main():
         print("ERROR: Unable to load 'config.yml'!")
         print("Please make sure the file exists and contains valid YAML")
 
+    # Remove dest if already exist
+    if os.path.isdir('dest'):
+        try:
+            rmtree('dest')
+        except Exception as e:
+            print("ERROR: Unable to clean DEST!")
+            print(f"ERROR: {e}")
+            sys.exit(1)
 
+    # Copy src to dest
+    try:
+        copytree('src', 'dest')
+    except Exception as e:
+        print("ERROR: Unable to copy SRC to DEST!")
+        print(f"ERROR: {e}")
+        sys.exit(1)
+
+    # Get files
 
     pDict = {
         'site': config,
@@ -54,5 +138,10 @@ def main():
     }
 
     print(renderTemplate('default', 'templates', pDict))
+
+    # scanPage test
+    print(scanPage('src/index.md'))
+
+    print(getFiles('src'))
 
 main()
